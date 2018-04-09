ce99bcd815151b7d61c253f8b36ce5b28270f8e0
Jakub Ledzion <ometerii@op.pl>
2018-04-09 21:08:28 +0200

    To add project

---
+++ lepasi/Controllers/StatusController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using lepasi;
+
+namespace lepasi.Controllers
+{
+    public class StatusController : Controller
+    {
+        private MotoJanuszEntities db = new MotoJanuszEntities();
+
+        // GET: Status
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.status.ToListAsync());
+        }
+
+        // GET: Status/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            status status = await db.status.FindAsync(id);
+            if (status == null)
+            {
+                return HttpNotFound();
+            }
+            return View(status);
+        }
+
+        // GET: Status/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Status/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ST_id,ST_nazwa")] status status)
+        {
+            if (ModelState.IsValid)
+            {
+                db.status.Add(status);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(status);
+        }
+
+        // GET: Status/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            status status = await db.status.FindAsync(id);
+            if (status == null)
+            {
+                return HttpNotFound();
+            }
+            return View(status);
+        }
+
+        // POST: Status/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ST_id,ST_nazwa")] status status)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(status).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(status);
+        }
+
+        // GET: Status/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            status status = await db.status.FindAsync(id);
+            if (status == null)
+            {
+                return HttpNotFound();
+            }
+            return View(status);
+        }
+
+        // POST: Status/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            status status = await db.status.FindAsync(id);
+            db.status.Remove(status);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ lepasi/Models/TypViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCTutorial.Models
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public Nullable<int> DepartmentId { get; set; }
+        public string Address { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+
+        //Custom attribute
+        public string DepartmentName { get; set; }
+        public bool Remember { get; set; }
+        public string SiteName { get; set; }
+       
+    }
+}
